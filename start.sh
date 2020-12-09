a787156b8b064567c9b9c1c62d9b9090c3cd7f03
Jan Berge Sommerdahl <jan@jotta.no>
2020-12-09 19:21:16 +0100

    retry git fetch for some time

--- start.sh
+++ start.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-set euxo pipefail
 echo "starting james"
 
-git fetch
+REMAINING_FETCHES=20
+while [ ${REMAINING_FETCHES} -gt 0 ]; do
+  echo "${REMAINING_FETCHES}: git fetch"
+  git fetch
+  if [ $? == 0 ]; then
+    REMAINING_FETCHES=0
+  else
+    REMAINING_FETCHES=$((REMAINING_FETCHES - 1))
+    sleep 5
+  fi
+done
+
 git checkout origin/master
 npm install
 authbind --deep npm start
